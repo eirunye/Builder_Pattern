0d66ef79b218ead1150bed08bebbffac4f028dc4
yrung <yrungen@163.com>
2018-12-03 23:06:28 +0800

    fix bug

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+
         Product product = Product.builder().setName("零吃").build();
         System.out.println(product.getName());
     }
